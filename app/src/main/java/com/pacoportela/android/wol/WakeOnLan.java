4e0c68a64e4147909a06302969f9c9b8de751853
pacoportelahenche <portelahenche@gmail.com>
2017-03-28 21:54:58 +0200

    Initial commit

---
+++ app/src/main/java/com/pacoportela/android/wol/WakeOnLan.java
@@ -0,0 +1,13 @@
+package com.pacoportela.android.wol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WakeOnLan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wake_on_lan);
+    }
+}
